b7a745c6e7e7cf93899a026a26a34483a3ec18d5
mhg16 <hammahadi20006@gmail.com>
2022-04-14 23:43:03 +0200

    add api key authorization

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ from flask_restx import Api
 
 from language.views import api_book
 
+
+authorizations = {"Bearer": {"type": "apikey", "in": "header", "name": "X-API-KEY"}}
+
 app = Flask(__name__)
 blueprint = Blueprint("Api", __name__, url_prefix="/swagger")
 api = Api(
@@ -11,12 +14,13 @@ api = Api(
     doc="/documentation",
     title="Language Api",
     description="Tuto Api documentation",
+    authorizations=authorizations,
+    security="apikey",
 )
 app.register_blueprint(blueprint)
 api.add_namespace(api_book)
 
 app.config["SWAGGER_UI_JSONEDITOR"] = True
-
 
 if __name__ == "__main__":
     app.run(debug=True)
--- language/views.py
+++ language/views.py
@@ -1,3 +1,6 @@
+from functools import wraps
+
+from flask import request
 from flask_restx import fields
 from flask_restx import Namespace
 from flask_restx import Resource
@@ -18,19 +21,46 @@ lang_model = api_book.model(
     },
 )
 
+parser = api_book.parser()
+parser.add_argument("X-API-KEY", location="headers")
 
+
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+
+        token = None
+
+        if "X-API-KEY" in request.headers:
+            token = request.headers["X-API-KEY"]
+
+        if not token:
+            return {"message": "Token is missing"}, 401
+
+        if token != "myToken":
+            return {"message": "Your token is wrong, wrong, wrong !!!"}, 401
+        return f(*args, **kwargs)
+
+    return decorated
+
+
+@api_book.doc(
+    responses={
+        200: "Success",
+        401: "token missing or token wrong",
+        400: "Error connection database",
+    },
+    security="apiKey",
+)
+@api_book.header("X-API-KEY")
 @api_book.route("/languages")
+@api_book.expect(parser)
 class Languages(Resource):
-    @api_book.doc(
-        responses={
-            200: "Success",
-            401: "Error internal server",
-            400: "Error connection database",
-        }
-    )
+    @token_required
     def get(self):
         return languages
 
+    @token_required
     @api_book.expect(lang_model)
     def post(self):
         Langage.insert(languages=languages)
